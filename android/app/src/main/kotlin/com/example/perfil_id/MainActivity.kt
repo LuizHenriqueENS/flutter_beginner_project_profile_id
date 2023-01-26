630efaca6f3c2f69c2f73a5675baa0d3c1d12466
Luiz HenriqueENS <luizhenrique.egydio@gmail.com>
2023-01-26 12:57:49 -0300

    finished project

---
+++ android/app/src/main/kotlin/com/example/perfil_id/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.perfil_id
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: ProfileCard(),
+  ));
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      appBar: AppBar(
+        title: const Text('Mount Olympos ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[700],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/heracles.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
+            const Text(
+              'NAME:',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              'Herácles',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30.0),
+            const Text(
+              'CURRENT POWER LEVEL',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              '150',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(Icons.email, color: Colors.grey[400]),
+                const SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'heracles.son.ofzeus@olympos.gr',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
